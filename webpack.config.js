9c1908424ea83a6438fe7b35eed4eb6a9200a84e
Alejandro Sulbaran <ersulba@gmail.com>
2020-01-23 21:00:09 -0300

    fix error

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
       template: path.join(__dirname, 'src', 'index.html'),
     }),
   ],
-<<<<<<< HEAD
   module: {
     //aca establecemos las reglas para cargar nuestros modulos
     rules: [
@@ -31,6 +30,4 @@ module.exports = {
       },
     ],
   },
-=======
->>>>>>> fa88c36e777a9b5c6a8822b3ce44d97c69a10518
 }
